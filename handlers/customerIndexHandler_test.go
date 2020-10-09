a392374240b4e0253127562ff6adb596c1b1ca7e
Ken <kwill1253@gmail.com>
2020-10-08 20:38:02 -0400

    updates to coverage

--- handlers/customerIndexHandler_test.go
+++ handlers/customerIndexHandler_test.go
@@ -123,3 +123,118 @@ func TestSix910Handler_Index(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestSix910Handler_Index2(t *testing.T) {
+	var sh Six910Handler
+	var l lg.Logger
+	l.LogLevel = lg.AllLevel
+	sh.Log = &l
+	var sapi mapi.MockAPI
+	sapi.SetStoreID(59)
+
+	sapi.SetRestURL("http://localhost:3002")
+	sapi.SetStore("defaultLocalStore", "defaultLocalStore.mydomain.com")
+	sapi.SetAPIKey("GDG651GFD66FD16151sss651f651ff65555ddfhjklyy5")
+	sh.API = &sapi
+
+	//-----------start mocking------------------
+
+	var prod sdbi.Product
+	prod.ID = 1
+	prod.Sku = "1"
+	prod.Desc = "test"
+
+	var prod2 sdbi.Product
+	prod2.ID = 2
+	prod2.Sku = "2"
+	prod2.Desc = "test"
+
+	var prod3 sdbi.Product
+	prod3.ID = 3
+	prod3.Sku = "3"
+	prod3.Desc = "test"
+
+	var prod4 sdbi.Product
+	prod4.ID = 4
+	prod4.Sku = "4"
+	prod4.Desc = "test"
+
+	var prod5 sdbi.Product
+	prod5.ID = 5
+	prod5.Sku = "5"
+	prod5.Desc = "test"
+
+	var plst []sdbi.Product
+	plst = append(plst, prod)
+	plst = append(plst, prod2)
+	plst = append(plst, prod3)
+	plst = append(plst, prod4)
+	plst = append(plst, prod5)
+	sapi.MockProductList = &plst
+
+	//-----------end mocking --------
+
+	var c conts.CmsService
+	var cds ds.DataStore
+	cds.Path = "../contentsrv/testFiles"
+	//ds.Delete("books1")
+	c.Log = &l
+	c.Store = cds.GetNew()
+
+	var ct conts.Content
+	ct.Name = "index"
+	ct.Author = "ken"
+	ct.MetaAuthorName = "ken"
+	ct.MetaDesc = "shopping cart index"
+	ct.Text = "some book text"
+	ct.Title = "the best book ever"
+	ct.Visible = true
+	res := c.AddContent(&ct)
+	fmt.Println("content save: ", res)
+
+	sh.ContentService = c.GetNew()
+
+	var sms musrv.Six910MenuService
+	var mds ds.DataStore
+	mds.Path = "./testFiles"
+	sms.MenuStore = mds.GetNew()
+	sms.Log = &l
+	ms := sms.GetNew()
+
+	var m musrv.Menu
+	m.Name = "menu1"
+	m.Active = true
+	m.Location = "top"
+	m.Shade = "light"
+	m.Background = "light"
+	m.Style = ""
+	m.ShadeList = &[]string{"light", "dark"}
+	m.BackgroundList = &[]string{"light", "dark"}
+
+	msuc := ms.AddMenu(&m)
+	fmt.Println("menu save: ", msuc)
+
+	sh.MenuService = ms
+
+	var cc ClientCreds
+	cc.AuthCodeState = "123"
+	sh.ClientCreds = &cc
+	sh.ClientCreds.AuthCodeClient = "1"
+	sh.OauthHost = "test.com"
+
+	sh.Templates = template.Must(template.ParseFiles("testHtmls/test.html"))
+
+	r, _ := http.NewRequest("POST", "https://test.com", nil)
+	w := httptest.NewRecorder()
+	s, suc := sh.getSession(r)
+	fmt.Println("suc: ", suc)
+	//s.Values["loggedIn"] = true
+	s.Save(r, w)
+	h := sh.GetNew()
+	h.Index(w, r)
+	fmt.Println("code: ", w.Code)
+
+	if w.Code != 200 {
+		t.Fail()
+	}
+}
