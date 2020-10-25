de813f6ffb07ab22a63d39567a841f0b7875972e
Ken <kwill1253@gmail.com>
2020-10-24 20:31:39 -0400

    updates to coverage

--- handlers/customerCartHandler_test.go
+++ handlers/customerCartHandler_test.go
@@ -859,7 +859,7 @@ func TestSix910Handler_CheckOutView(t *testing.T) {
 	c.Store = cds.GetNew()
 
 	var ct conts.Content
-	ct.Name = "product"
+	ct.Name = "shoppingCart2"
 	ct.Author = "ken"
 	ct.MetaAuthorName = "ken"
 	ct.MetaDesc = "shopping cart index"
@@ -868,6 +868,156 @@ func TestSix910Handler_CheckOutView(t *testing.T) {
 	ct.Visible = true
 	res := c.AddContent(&ct)
 	fmt.Println("content save: ", res)
+
+	sh.ContentService = c.GetNew()
+
+	var sms musrv.Six910MenuService
+	var mds ds.DataStore
+	mds.Path = "./testFiles"
+	sms.MenuStore = mds.GetNew()
+	sms.Log = &l
+	ms := sms.GetNew()
+
+	var mm musrv.Menu
+	mm.Name = "menu1"
+	mm.Active = true
+	mm.Location = "top"
+	mm.Shade = "light"
+	mm.Background = "light"
+	mm.Style = ""
+	mm.ShadeList = &[]string{"light", "dark"}
+	mm.BackgroundList = &[]string{"light", "dark"}
+
+	msuc := ms.AddMenu(&mm)
+	fmt.Println("menu save: ", msuc)
+
+	sh.MenuService = ms
+
+	var css csssrv.Six910CSSService
+	var csds ds.DataStore
+	csds.Path = "./testFiles"
+	css.CSSStore = csds.GetNew()
+	css.Log = &l
+	sh.CSSService = css.GetNew()
+
+	var cc ClientCreds
+	cc.AuthCodeState = "123"
+	sh.ClientCreds = &cc
+	sh.ClientCreds.AuthCodeClient = "1"
+	sh.OauthHost = "test.com"
+
+	sh.Templates = template.Must(template.ParseFiles("testHtmls/test.html"))
+
+	r, _ := http.NewRequest("POST", "https://test.com", nil)
+	vars := map[string]string{
+		"prodId":   "9",
+		"quantity": "2",
+	}
+
+	var cilstp []sdbi.CartItem
+
+	var ctit1 sdbi.CartItem
+	ctit1.Quantity = 3
+	ctit1.ProductID = 7
+	cilstp = append(cilstp, ctit1)
+
+	var ctit2 sdbi.CartItem
+	ctit2.Quantity = 4
+	ctit2.ProductID = 9
+	cilstp = append(cilstp, ctit2)
+
+	var cccs m.CustomerCart
+	cccs.Items = &cilstp
+
+	r = mux.SetURLVars(r, vars)
+	w := httptest.NewRecorder()
+	s, suc := sh.getUserSession(r)
+	fmt.Println("suc: ", suc)
+	s.Values["loggedIn"] = true
+	s.Values["customerUser"] = true
+	s.Values["customerId"] = 55
+	s.Values["customerCart"] = &cccs
+	s.Save(r, w)
+	h := sh.GetNew()
+	h.CheckOutView(w, r)
+	fmt.Println("code: ", w.Code)
+
+	if w.Code != 200 {
+		t.Fail()
+	}
+}
+
+func TestSix910Handler_CheckOutView2(t *testing.T) {
+	var sh Six910Handler
+	var l lg.Logger
+	l.LogLevel = lg.AllLevel
+	sh.Log = &l
+	var sapi mapi.MockAPI
+	sapi.SetStoreID(59)
+
+	sapi.SetRestURL("http://localhost:3002")
+	sapi.SetStore("defaultLocalStore", "defaultLocalStore.mydomain.com")
+	sapi.SetAPIKey("GDG651GFD66FD16151sss651f651ff65555ddfhjklyy5")
+	sh.API = &sapi
+
+	var man m.Six910Manager
+	man.API = &sapi
+	sh.API = &sapi
+	man.Log = &l
+	sh.Manager = man.GetNew()
+
+	//-----------start mocking------------------
+
+	var prod sdbi.Product
+	prod.ID = 2
+	prod.Desc = "test"
+	sapi.MockProduct = &prod
+
+	var pg sdbi.PaymentGateway
+	pg.ID = 2
+	pg.StorePluginsID = 4
+	var pglst []sdbi.PaymentGateway
+	pglst = append(pglst, pg)
+	sapi.MockPaymentGatewayList = &pglst
+
+	var spi sdbi.StorePlugins
+	spi.ID = 4
+	spi.PluginName = "PAYPAL"
+	sapi.MockStorePlugin = &spi
+
+	var sm sdbi.ShippingMethod
+	sm.ID = 5
+	sm.Name = "USP"
+	var smslt []sdbi.ShippingMethod
+	smslt = append(smslt, sm)
+	sapi.MockShippingMethodList = &smslt
+
+	var ins sdbi.Insurance
+	ins.ID = 4
+	ins.Cost = 4.55
+	var inslst []sdbi.Insurance
+	inslst = append(inslst, ins)
+	sapi.MockInsuranceList = &inslst
+
+	//-----------end mocking --------
+
+	var c conts.CmsService
+	var cds ds.DataStore
+	cds.Path = "../contentsrv/testFiles"
+	cds.Delete("shoppingCart2")
+	c.Log = &l
+	c.Store = cds.GetNew()
+
+	var ct conts.Content
+	ct.Name = "shoppingCart2"
+	ct.Author = "ken"
+	ct.MetaAuthorName = "ken"
+	ct.MetaDesc = "shopping cart index"
+	ct.Text = "some book text"
+	ct.Title = "the best book ever"
+	ct.Visible = true
+	//res := c.AddContent(&ct)
+	//fmt.Println("content save: ", res)
 
 	sh.ContentService = c.GetNew()
 
