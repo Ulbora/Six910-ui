39fcb9198762c47cb4d08b3f9aaf3dd648d57ca7
Ken <kwill1253@gmail.com>
2023-04-12 20:22:31 -0400

    update

---
+++ handlers/test.sh
@@ -0,0 +1,59 @@
+cd bkupsrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd carouselsrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd contentsrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd countrysrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd csssrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd imgsrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd mailsrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd menusrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd statesrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd templatesrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd usersrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd findfflsrv
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd managers
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+cd handlers
+go test -coverprofile=coverage.out
+sleep 15
+cd ..
+# cd compresstoken
+# go test -coverprofile=coverage.out
+# sleep 15
